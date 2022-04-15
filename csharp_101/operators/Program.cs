7db3120a87f2550a968c6ec842d696ed9c9a5d23
Abdussamed Dogan <abdussamed.dgn@hotmail.com>
2022-04-15 23:42:35 +0300

    Operatos Commit 1

--- csharp_101/operators/Program.cs
+++ csharp_101/operators/Program.cs
@@ -9,7 +9,36 @@ namespace operators
             
             int b = 5;
             b +=5;
+            Console.WriteLine(b);
             b /=2;
+            Console.WriteLine(b);
+
+            // || (veya), && (ve), ! (değilse) Mantıksal oparetörler
+
+            bool isSuccess = true;
+            bool isComplated = false;
+
+            if(isSuccess && isComplated)
+            {
+                Console.WriteLine("Nice!");
+            } 
+            if(isSuccess && !isComplated)
+            {
+                Console.WriteLine("Perfect!");
+            } 
+            if(isSuccess || isComplated)
+            {
+                Console.WriteLine("Amazing!");
+            } 
+
+            // Fiziksel operatörler
+            // <, >, >=, <=, ==, !=
+
+            int f1 = 5;
+            int f2 = 8;
+
+             
+             
         }
     }
 }
