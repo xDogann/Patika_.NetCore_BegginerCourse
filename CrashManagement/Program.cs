249266e5f1d275f52fb2d94907fa86dd0e3ce361
Abdussamed Dogan <abdussamed.dgn@hotmail.com>
2022-06-11 04:00:02 +0300

    w

--- CrashManagement/Program.cs
+++ CrashManagement/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CrashManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\n--> Assignment and Operational Assignment Operators");
+
+            int b = 5;
+            b +=5;
+            Console.WriteLine(b);
+            b /=2;
+            Console.WriteLine(b);
+
+
+            // || (veya), && (ve), ! (değilse) Mantıksal oparetörler
+            Console.WriteLine("\n--> Logical Operators");
+
+            bool isSuccess = true;
+            bool isComplated = false;
+
+            if(isSuccess && isComplated)
+            {
+                Console.WriteLine("Nice!");
+            } 
+            if(isSuccess && !isComplated)
+            {
+                Console.WriteLine("Perfect!");
+            } 
+            if(isSuccess || isComplated)
+            {
+                Console.WriteLine("Amazing!");
+            } 
+
+             
+        }
+    }
+}
